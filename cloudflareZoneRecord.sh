b38d6be854329cc1dfd5d0cda9b1f76f4f9ad74a
Paul Komurka <pawlex@gmail.com>
2018-12-15 17:22:43 -0800

    added add/delete record

--- cloudflareZoneRecord.sh
+++ cloudflareZoneRecord.sh
@@ -30,14 +30,6 @@ if [ -e ./config.cf ]; then
     source ./config.cf
 fi
 
-# REQUIRED
-#EMAIL="user@email"         # CloudFlare account ID
-#GLOBAL_API_KEY="apikey"    # CloudFlare account API Key
-#ZONEID="zonekey"           # CloudFlare zone key
-# OPTIONAL
-#DYNDNSRECORDID="700f7b170dcd84adasdadadas"
-#DYNDNSRECORDNAME="foo.bar.baz"
-#DYNDNSRECORDTYPE="A"
 
 function formatJson() {
     # Reformat output if json_reformat exists
@@ -94,64 +86,130 @@ function getZone() {
     printf "$RETVAL\n"
 }
 
+# CREATE ZONE RECORD
+function addRecord() {
+    #POST https://api.cloudflare.com/client/v4/zones/d2b4e568601e72a510503a5015eb4c18/dns_records
+    RECORDTYPE=$1
+    RECORDNAME=$2
+    RECORDVALUE=$3
+    #
+    RETVAL=$($CURL -s -X POST \
+        "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records" \
+        -H "X-Auth-Email: $EMAIL" \
+        -H "X-Auth-Key: $GLOBAL_API_KEY" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"$RECORDTYPE\",\"name\":\"$RECORDNAME\",\"content\":\"$RECORDVALUE\",\"ttl\":120,\"priority\":10,\"proxied\":false}")
+    #
+    if [[ "$RETVAL" == *'"success":true'* ]]; then
+        echo "SUCCESS"
+        exit 0
+    else
+        echo "FAILED"
+        printf "$RETVAL\n"
+        exit -1
+    fi
+}
+
+function deleteRecord() {
+    RECORDID=$1
+    #
+    RETVAL=$($CURL -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$RECORDID" \
+         -H "X-Auth-Email: $EMAIL" \
+         -H "X-Auth-Key: $GLOBAL_API_KEY" \
+         -H "Content-Type: application/json")
+    #
+    if [[ "$RETVAL" == *'"success":true'* ]]; then
+        echo "SUCCESS"
+        exit 0
+    else
+        echo "FAILED"
+        printf "$RETVAL\n"
+        exit -1
+    fi
+}
 
 ##  PROGRAM ARGS
 case "$1" in
 
-GETZONE)
-        getZone
-;;
+    GETZONE)
+            getZone
+    ;;
 
-GETRECORD) if [ $# -lt 3 ]
-        then
-            echo "Usage : $0 $1 <RECORD TYPE: EG AAAA> <RECORDNAME: EG www.domain.com>"
-            exit
+    GETRECORD) if [ $# -lt 2 ]
+            then
+                echo "Usage : $0 $1 <RECORD TYPE: EG AAAA> <RECORDNAME: EG www.domain.com>"
+                exit
+            fi
+            getRecord $2 $3
+    ;;
+
+    SETRECORD) if [ $# -lt 4 ]
+           then
+                echo
+                echo "  Usage  : $1 <id*> <type> <name> <value>"
+                echo "  Example: $1 94a30abbca443e03fd11aa00ca74627e AAAA test.mydomain.org 127.0.0.1"
+                echo "      * record id can found by running GETZONE"
+                echo
+                exit -1
+            fi
+
+            setRecord $2 $3 $4 $5
+    ;;
+
+    ADDRECORD) if [ $# -lt 3 ]
+           then
+                echo
+                echo "  Usage  : $1 <type> <name> <value>"
+                echo "  Example: $1 AAAA test.mydomain.org 127.0.0.1"
+                echo
+                exit -1
+            fi
+
+        addRecord $2 $3 $4
+    ;;
+
+    DELRECORD) if [ $# -lt 2 ]
+           then
+                echo
+                echo "  Usage  : $1 <id*>"
+                echo "  Example: $1 94a30abbca443e03fd11aa00ca74627e"
+                echo "      * record id can found by running GETZONE"
+                echo
+                exit -1
+            fi
+
+        deleteRecord $2
+    ;;
+
+    DYNDNSUPDATE)
+
+        if [ -z "$DYNDNSRECORDID" ] || [ -z "$DYNDNSRECORDNAME" ] || [ -z "$DYNDNSRECORDTYPE" ]; then
+            printf "\$DYNDNSRECORDID, \$DYNDNSRECORDNAME, \$DYNDNSRECORDTYPE not defined!\n"
+            exit -1
         fi
-        getRecord $2 $3
-;;
-
-SETRECORD) if [ $# -lt 5 ]
-       then
-            echo
-            echo "  Usage  : $1 <id*> <type> <name> <value>"
-            echo "  Example: $1 94a30abbca443e03fd11aa00ca74627e AAAA test.mydomain.org 127.0.0.1"
-            echo "      * record id can found by running GETZONE"
-            echo
+        
+        if hash host 2>/dev/null; then
+            HOST=`which host`
+        else
+            printf "host not installed.  cannot resolve $DYNDNSRECORDNAME"
             exit -1
         fi
 
-        setRecord $2 $3 $4 $5
-;;
+        ## GRAB CURRENT LOCAL IP ADDRESS and PREVIOUS IP address from cloudflare
+        CURRENT_IP=$($CURL -s checkip.dyndns.org | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
+        PREVIOUS_IP=$($HOST -4 $DYNDNSRECORDNAME ken.ns.cloudflare.com | grep "has address" | cut -d" " -f4)
+        
+        if [ $CURRENT_IP == $PREVIOUS_IP ]; then
+            echo "NO UPDATE NECESSARY"
+        else
+            echo "UPDATE NECESSARY"
+            echo "$PREVIOUS_IP -> $CURRENT_IP"
+            setRecord $DYNDNSRECORDID $DYNDNSRECORDTYPE $DYNDNSRECORDNAME $CURRENT_IP
+        fi
+    ;;
 
-DYNDNSUPDATE)
-
-    if [ -z "$DYNDNSRECORDID" ] || [ -z "$DYNDNSRECORDNAME" ] || [ -z "$DYNDNSRECORDTYPE" ]; then
-        printf "\$DYNDNSRECORDID, \$DYNDNSRECORDNAME, \$DYNDNSRECORDTYPE not defined!\n"
-        exit -1
-    fi
-    
-    if hash host 2>/dev/null; then
-        HOST=`which host`
-    else
-        printf "host not installed.  cannot resolve $DYNDNSRECORDNAME"
-        exit -1
-    fi
-
-    ## GRAB CURRENT LOCAL IP ADDRESS and PREVIOUS IP address from cloudflare
-    CURRENT_IP=$($CURL -s checkip.dyndns.org | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
-    PREVIOUS_IP=$($HOST -4 $DYNDNSRECORDNAME ken.ns.cloudflare.com | grep "has address" | cut -d" " -f4)
-    
-    if [ $CURRENT_IP == $PREVIOUS_IP ]; then
-        echo "NO UPDATE NECESSARY"
-    else
-        echo "UPDATE NECESSARY"
-        echo "$PREVIOUS_IP -> $CURRENT_IP"
-        setRecord $DYNDNSRECORDID $DYNDNSRECORDTYPE $DYNDNSRECORDNAME $CURRENT_IP
-    fi
-;;
-
-*) echo echo "Usage : $0 <COMMAND> <RECORD>"
-;;
+    *) echo echo "Usage : $0 <COMMAND> <RECORD>"
+    ;;
 
 esac
 
